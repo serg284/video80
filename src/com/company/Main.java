afc1679fbcec474c29ddb0fdb07036648e23e546
Sergio <serg.ribeiro@gmail.com>
2015-12-27 01:42:17 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+//video80 , default
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello1");
+
+    createfile g = new createfile();
+        g.openFile();
+        g.addRecords();
+        g.closeFile();
+
+    }
+}
---
+++ src/com/company/createfile.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class createfile {
+
+    private Formatter x;
+    public void openFile(){
+        try{
+            x = new Formatter("output1.txt");
+        }
+        catch(Exception e){
+            System.out.println("you have an error");
+        }
+
+    }
+
+    public void addRecords(){
+        x.format("%s %s %s", "20", "bucky", "roberts");
+    }
+
+    public void closeFile(){
+        x.close();
+    }
+
+}
